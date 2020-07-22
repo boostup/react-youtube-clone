e59f862e49cd8a10fcc61c9e6a18d5fa68f7e76c
boostup <boostup@gmail.com>
2020-07-22 10:45:24 +0200

    added router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CssBaseline } from "@material-ui/core";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Router>
+    <CssBaseline />
+    <App />
+  </Router>,
+  document.querySelector("#root")
+);
